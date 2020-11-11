701d41b17de6dcee3388acf6dd1a9afc872ef8b8
Salome Kuitca <salokuitca@gmail.com>
2020-11-11 16:18:10 -0300

    Versión Final

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-
-$(window).load(function() {
-    $(".loader").fadeOut("slow");
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
 });
 
-
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-}
-)
-
 function promo() {
-    $('#descripcion-dire').val('Promo Aplicada: "Piquibola" \n -----------------------------------------\n')
+    $('#descripcion-dire').val('Promo Aplicada: "Piquibola" \n -----------------------------\n')
 }
